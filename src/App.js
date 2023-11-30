756de8217f7a76a949e5f2b48b196b27f9c6416b
Apoorv Trivedi <115005135+apoorv1896@users.noreply.github.com>
2023-11-30 11:37:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from './Component/Header';
+import Body from './Component/Body';
+import Footer from './Component/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer bg-gray-600 text-center p-8' >
+      <p>Copyright 2023 Made By Priyam All right reserved</p>
+    </div>
+  )
+}
+
+export default Footer
